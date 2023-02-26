8d9d94e9de21c152bc3497ca61193c3d9a17bdfc
Rdinaft <rdinaft@mail.ru>
2023-02-26 13:19:16 +0300

    Working with lists and dictionaries

---
+++ company.py
@@ -0,0 +1,63 @@
+"""
+В этот раз у нас есть компания, в ней отделы, в отделах люди. У людей есть имя, должность и зарплата.
+Ваши задачи такие:
+1. Вывести названия всех отделов
+2. Вывести имена всех сотрудников компании.
+3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают.
+4. Вывести имена всех сотрудников компании, которые получают больше 100к.
+5. Вывести позиции, на которых люди получают меньше 80к (можно с повторениями).
+6. Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела
+"""
+
+departments = [
+    {
+        "title": "HR department",
+        "employers": [
+            {"first_name": "Daniel", "last_name": "Berger", "position": "Junior HR", "salary_rub": 50000},
+            {"first_name": "Michelle", "last_name": "Frey", "position": "Middle HR", "salary_rub": 75000},
+            {"first_name": "Kevin", "last_name": "Jimenez", "position": "Middle HR", "salary_rub": 70000},
+            {"first_name": "Nicole", "last_name": "Riley", "position": "HRD", "salary_rub": 120000},
+        ]
+    },
+    {
+        "title": "IT department",
+        "employers": [
+            {"first_name": "Christina", "last_name": "Walker", "position": "Python dev", "salary_rub": 80000},
+            {"first_name": "Michelle", "last_name": "Gilbert", "position": "JS dev", "salary_rub": 85000},
+            {"first_name": "Caitlin", "last_name": "Bradley", "position": "Teamlead", "salary_rub": 950000},
+            {"first_name": "Brian", "last_name": "Hartman", "position": "CTO", "salary_rub": 130000},
+        ]
+    },
+]
+
+for department in departments:  # 1
+    print(department['title'])
+
+
+for department in departments:  # 2
+    for worker in department['employers']:
+        print(worker['first_name'])
+
+
+for department in departments:  # 3
+    for worker in department['employers']:
+        print(worker['first_name'], department['title'])
+
+
+for department in departments:  # 4
+    for worker in department['employers']:
+        if worker['salary_rub'] > 100000:
+            print(worker['first_name'])
+
+
+for department in departments:  # 5
+    for worker in department['employers']:
+        if worker['salary_rub'] < 80000:
+            print(worker['first_name'])
+
+
+for department in departments:  # 6
+    sum_waste = 0
+    for worker in department['employers']:
+        sum_waste += worker['salary_rub']
+    print(department['title'], sum_waste)
